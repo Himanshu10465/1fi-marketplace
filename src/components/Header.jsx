913b2a5ae6f315ca4e4e8b2464f5415113e11bd1
Himanshu10465 <himanshumishra10465@gmail.com>
2026-09-06 22:41:04 +0530

    Add Header component for marketplace

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="marketplace-header">
+      <h2>1Fi Marketplace</h2>
+      <input type="text" placeholder="Search products..." />
+    </div>
+  );
+}
